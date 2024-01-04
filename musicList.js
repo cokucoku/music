18e5e790b4b358158fcd19298969e47dbda80440
leeao <34832462@qq.com>
2024-01-04 09:23:12 +0800

    t

--- musicList.js
+++ musicList.js
@@ -1,7 +1,7 @@
 {
     list: [
         {
-            songUrl: 'https://lx-sycdn.kuwo.cn/17270c4352fd916f22137eead62e82d8/659515fc/resource/n3/87/48/2437338318.mp3',
+            songUrl: 'lrc/01-hgs.mp3',
             songName: '胡广生',
             songAuto: '任素夕',
             duration: 215,
@@ -10,7 +10,7 @@
             ok: '01-hgs-ok.lrc'
         },
         {
-            songUrl: 'https://lv-sycdn.kuwo.cn/60bc3d48d05d6002e0c8441f22e2c972/65951661/resource/30106/trackmedia/M800000HY8ha2kgK2l.mp3',
+            songUrl: 'lrc/02-mq.mp3',
             songName: '麻雀',
             songAuto: '李荣浩',
             duration: 252,
@@ -19,7 +19,7 @@
             ok: '02-mq-ok.lrc'
         },
         {
-            songUrl: 'https://ci-sycdn.kuwo.cn/0a1e7d6f5cfaca64cad12cc58112f3d4/659519ad/resource/n1/95/51/2479305406.mp3',
+            songUrl: 'lrc/03-lzy.mp3',
             songName: '庐州月',
             songAuto: '许嵩',
             duration: 255,
@@ -28,7 +28,7 @@
             ok: '03-lzy-ok.lrc'
         },
         {
-            songUrl: 'https://cp-sycdn.kuwo.cn/eceb46e10389fbb38dcb4b78b8759f58/659519de/resource/n2/92/33/2946818726.mp3',
+            songUrl: 'lrc/04-gl.mp3',
             songName: '光亮',
             songAuto: '周深',
             duration: 288,
@@ -37,7 +37,7 @@
             ok: '04-gl-ok.lrc'
         },
         {
-            songUrl: 'https://ra-sycdn.kuwo.cn/059a04338c8599d1d68fb90ee1bed7f4/65951a21/resource/n3/320/50/57/3889578767.mp3',
+            songUrl: 'lrc/05-xhn.mp3',
             songName: '喜欢你',
             songAuto: 'beyond',
             duration: 276,
@@ -46,7 +46,7 @@
             ok: '05-xhn-ok.lrc'
         },
         {
-            songUrl: 'https://lv-sycdn.kuwo.cn/b03f78b815bd7ec0478c72d24bbffca2/65951a80/resource/30106/trackmedia/M800000zgxO01IEuV5.mp3',
+            songUrl: 'lrc/06-kdzyddf.mp3',
             songName: '看得最远的地方',
             songAuto: '毛不易',
             duration: 252,
@@ -55,7 +55,7 @@
             ok: '06-kdzyddf-ok.lrc'
         },
         {
-            songUrl: 'https://lw-sycdn.kuwo.cn/6ce8f8c59d2c8c532822232bae253fe1/65951abe/resource/30106/trackmedia/M800000IZDO70yZWXT.mp3',
+            songUrl: 'lrc/07-zypf.mp3',
             songName: '只要平凡',
             songAuto: '张杰&张碧晨',
             duration: 246,
@@ -64,7 +64,7 @@
             ok: '07-zypf-ok.lrc'
         },
         {
-            songUrl: 'https://lq-sycdn.kuwo.cn/270bf9513362f3b698f223c3d7f3548d/65951b15/resource/n3/81/96/2862913863.mp3',
+            songUrl: 'lrc/08-hnyy.mp3',
             songName: '和你一样',
             songAuto: '李宇春',
             duration: 307,
@@ -73,7 +73,7 @@
             ok: '08-hnyy-ok.lrc'
         },
         {
-            songUrl: 'https://lx-sycdn.kuwo.cn/890e274b8c4b264a2fa2cfd5ebe7ef90/65951f54/resource/n1/63/74/3518998464.mp3',
+            songUrl: 'lrc/09-lxl.mp3',
             songName: '李香兰',
             songAuto: '张学友',
             duration: 396,
@@ -82,7 +82,7 @@
             ok: '09-lxl-ok.lrc'
         },
         {
-            songUrl: 'https://lx-sycdn.kuwo.cn/87678997af5641de7342013734643007/65951f86/resource/n2/60/37/805125701.mp3',
+            songUrl: 'lrc/10-bysh.mp3',
             songName: '不要说话',
             songAuto: '陈奕迅',
             duration: 288,
@@ -91,7 +91,7 @@
             ok: '10-bysh-ok.lrc'
         },
         {
-            songUrl: 'https://rh-sycdn.kuwo.cn/5e1e219bf05400d7fbe4fbe7848e53cb/65951ff7/resource/n3/66/37/3905628695.mp3',
+            songUrl: 'lrc/11-wack.mp3',
             songName: '为爱痴狂',
             songAuto: '金志文',
             duration: 142,
@@ -100,7 +100,7 @@
             ok: '11-wack-ok.lrc'
         },
         {
-            songUrl: 'https://ra-sycdn.kuwo.cn/66ae248da43793248ca2ca2da58d69be/65952058/resource/n1/128/13/98/1212471897.mp3',
+            songUrl: 'lrc/12-mtnh.mp3',
             songName: '明天，你好',
             songAuto: '牛奶咖啡',
             duration: 271,
@@ -109,7 +109,7 @@
             ok: '12-mtnh-ok.lrc'
         },
         {
-            songUrl: 'https://m801.music.126.net/20240103172142/d19fe24e5486f0d61ec3cf94d2d0f28e/jdyyaac/0052/045a/515e/78e8e61c4d3049d8d2c5f4b20dcefead.m4a',
+            songUrl: 'lrc/13-bjdt.mp3',
             songName: '北京的天',
             songAuto: '青蛙乐队',
             duration: 242,
@@ -118,7 +118,7 @@
             ok: '13-bjdt-ok.lrc'
         },
         {
-            songUrl: 'https://rm-sycdn.kuwo.cn/f5c56d6ca6e7e35003583baa2309c0bd/659521ef/resource/n1/10/3/3156382469.mp3',
+            songUrl: 'lrc/14-xaddf.mp3',
             songName: '相爱的地方',
             songAuto: '青蛙乐队',
             duration: 234,
@@ -127,7 +127,7 @@
             ok: '14-xaddf-ok.lrc'
         },
         {
-            songUrl: 'https://rh-sycdn.kuwo.cn/582945b023212aab08885bfb0e1651c0/65952290/resource/n2/71/67/733711099.mp3',
+            songUrl: 'lrc/15-mhwgo.mp3',
             songName: 'My Heart Will Go On',
             songAuto: '席琳·迪翁',
             duration: 300,
@@ -136,7 +136,7 @@
             ok: '15-mhwgo-ok.lrc'
         },
         {
-            songUrl: 'https://lw-sycdn.kuwo.cn/e5b667bcdec96315c44adc12372a2dde/659522c2/resource/30106/trackmedia/M500003MX5r10hR1mv.mp3',
+            songUrl: 'lrc/16-mybd.mp3',
             songName: 'Move Your Body',
             songAuto: 'Sia/Alan Walker',
             duration: 217,
@@ -145,7 +145,7 @@
             ok: '16-mybd-ok.lrc'
         },
         {
-            songUrl: 'https://er-sycdn.kuwo.cn/4db3878180fb50c2fab91e05c4e6c3d6/659523d1/resource/30106/trackmedia/long/M500003ia2Bd4M5DTh.mp3',
+            songUrl: 'lrc/17-rh.mp3',
             songName: '绒花',
             songAuto: '韩红',
             duration: 267,
@@ -154,7 +154,7 @@
             ok: '17-rh-ok.lrc'
         },
         {
-            songUrl: 'https://lu-sycdn.kuwo.cn/d08139ba075092731d60a982c4e38de7/65952418/resource/n1/30/79/2891700690.mp3',
+            songUrl: 'lrc/18-je.mp3',
             songName: '九儿',
             songAuto: '韩红',
             duration: 152,
@@ -163,7 +163,7 @@
             ok: '18-je-ok.lrc'
         },
         {
-            songUrl: 'https://m801.music.126.net/20240103174324/7ae4b31f3eab94f8b2efea26b9d15643/jdyyaac/5309/5509/5359/dc0992a5d6bcbb188b4ad3fb701c33d2.m4a',
+            songUrl: 'lrc/19-kdqr.mp3',
             songName: '口袋情人',
             songAuto: 'sky',
             duration: 208,
@@ -172,7 +172,7 @@
             ok: '19-kdqr-ok.lrc'
         },
         {
-            songUrl: 'https://lv-sycdn.kuwo.cn/9fa1082fe31c7e2130d720f65f77e39e/659526a0/resource/30106/trackmedia/M500003m2sQw02KtPM.mp3',
+            songUrl: 'lrc/20-qfl.mp3',
             songName: '起风了',
             songAuto: '周深',
             duration: 211,
@@ -181,7 +181,7 @@
             ok: '20-qfl-ok.lrc'
         },
         {
-            songUrl: 'https://cg-sycdn.kuwo.cn/fc2a85cf75b877d64be5d36524a6284e/659526fd/resource/n1/11/89/2888508920.mp3',
+            songUrl: 'lrc/21-bhl.mp3',
             songName: '白桦林',
             songAuto: '朴树',
             duration: 227,
@@ -190,7 +190,7 @@
             ok: '21-bhl-ok.lrc'
         },
         {
-            songUrl: 'https://lv-sycdn.kuwo.cn/24429d36278d4a5820a651ebff8d2692/6595274e/resource/30106/trackmedia/M500004eePuq3pHTsg.mp3',
+            songUrl: 'lrc/22-ycsl.mp3',
             songName: '一程山路',
             songAuto: '毛不易',
             duration: 197,
@@ -199,7 +199,7 @@
             ok: '22-ycsl-ok.lrc'
         },
         {
-            songUrl: 'https://ri-sycdn.kuwo.cn/c7915422b22c9872b417a267f6b450f7/65952788/resource/n2/85/55/2390481509.mp3',
+            songUrl: 'lrc/23-cd.mp3',
             songName: '成都',
             songAuto: '赵雷',
             duration: 328,
@@ -208,7 +208,7 @@
             ok: '23-cd-ok.lrc'
         },
         {
-            songUrl: 'https://nf-sycdn.kuwo.cn/6e018fd26a241994aef6cdd809ef21a9/659527a8/resource/n2/14/33/1484573667.mp3',
+            songUrl: 'lrc/24-lgh.mp3',
             songName: '泸沽湖',
             songAuto: '麻园诗人',
             duration: 246,
